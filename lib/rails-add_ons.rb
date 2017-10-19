b680ead6e00b36dc6e659be93c28c3b9ee72bfc4
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-10-19 15:30:24 +0200

    Make requirement for twitter_bootstrap_components_rails optional.

--- lib/rails-add_ons.rb
+++ lib/rails-add_ons.rb
@@ -1,6 +1,10 @@
 require 'haml-rails'
 require 'font-awesome-rails'
-require 'twitter_bootstrap_components_rails'
+begin
+  require 'twitter_bootstrap_components_rails'
+rescue LoadError
+  puts "[Rails AddOns]: WARNING - Could not require twitter_bootstrap_components_rails."
+end
 require 'simple_form'
 require 'responders' if Rails::VERSION::MAJOR > 3
 require 'rails-i18n'
