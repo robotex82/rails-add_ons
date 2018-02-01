09be324673afd966f7e42aca6285ca29c3fe0313
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-01 09:02:48 +0100

    Add optional kaminari support to resources controller (via ResourcesController::Kaminari).

---
+++ app/concerns/resources_controller/kaminari.rb
@@ -0,0 +1,15 @@
+module ResourcesController::Kaminari
+  def self.included(base)
+    base.helper_method :paginate?
+  end
+
+  def paginate?
+    true
+  end
+
+  private
+
+  def load_collection
+    @collection = load_collection_scope.page(params[:page])
+  end
+end
