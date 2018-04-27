042a6493f64ac2a3dea9ac7a9030a4bd0748a9c3
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-04-27 10:54:52 +0200

    Add per_page pagination size option to ResourcesController::WillPaginate module.

--- app/concerns/resources_controller/will_paginate.rb
+++ app/concerns/resources_controller/will_paginate.rb
@@ -1,4 +1,16 @@
 module ResourcesController
+  # Setting the pagination size in the controller:
+  #
+  #     # app/controllers/posts_controller.rb
+  #     class PostsController < ApplicationController
+  #       # ...
+  #       private
+  #
+  #       def per_page
+  #         15
+  #       end
+  #     end
+  #
   module WillPaginate
     extend ActiveSupport::Concern
 
@@ -13,7 +25,9 @@ module ResourcesController
     private
 
     def load_collection
-      @collection = load_collection_scope.paginate(page: params[:page])
+      options = { page: params[:page] }
+      options[:per_page] = per_page if respond_to?(:per_page, true)
+      @collection = load_collection_scope.paginate(options)
     end
   end
 end
