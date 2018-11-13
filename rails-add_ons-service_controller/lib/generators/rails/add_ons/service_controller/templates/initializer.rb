1da2ed6074b1d551187c7d4a8c0eca1183bf91ed
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-11-13 14:18:27 +0100

    Add missing configuration option to initializer.

--- rails-add_ons-service_controller/lib/generators/rails/add_ons/service_controller/templates/initializer.rb
+++ rails-add_ons-service_controller/lib/generators/rails/add_ons/service_controller/templates/initializer.rb
@@ -1,2 +1,7 @@
 Rails::AddOns::ServiceController.configure do |config|
+  # Set the base controller for service controllers.
+  #
+  # default: config.service_controller_base_class_name = '::ApplicationController'
+  #
+  config.service_controller_base_class_name = '::ApplicationController'
 end
